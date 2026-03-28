9a8d1c7a28365c5b77ea5448a0cb24fd95488f14
jdugan35 <jdugan35@gatech.edu>
2026-03-28 14:30:18 -0400

    first commit

---
+++ scripts/FTHelper.cs
@@ -0,0 +1,17 @@
+using Godot;
+
+namespace FTHelper
+{
+    public class Image
+    {
+        public enum ColorType
+        {
+            RGB,
+            LAB,
+        }
+
+        public void GetPixel() { }
+
+        public void SetPixel() { }
+    }
+}
