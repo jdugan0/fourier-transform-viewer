7fc7bef8dc867145d6117de4eae4bd77f64cb466
jdugan35 <jdugan35@gatech.edu>
2026-03-29 12:06:30 -0400

    updated image

--- scripts/FTHelper.cs
+++ scripts/FTHelper.cs
@@ -3,9 +3,19 @@ using Godot;
 
 namespace FTHelper
 {
+    public enum Channel
+    {
+        R,
+        G,
+        B,
+        L,
+        A,
+        Lab_B,
+    }
+
     public static class ColorConvert
     {
-        public static (double L, double A, double B) RGBToLAB(int r, int g, int b)
+        public static (double L, double A, double B) RGBToLAB(double r, double g, double b)
         {
             double rf = r / 255.0;
             double gf = g / 255.0;
@@ -30,7 +40,7 @@ namespace FTHelper
             return (L, A, B);
         }
 
-        public static (int R, int G, int B) LABToRGB(double L, double A, double B)
+        public static (double R, double G, double B) LABToRGB(double L, double A, double B)
         {
             double y = (L + 16.0) / 116.0;
             double x = A / 500.0 + y;
@@ -55,151 +65,214 @@ namespace FTHelper
             gf = gf > 0.0031308 ? 1.055 * Math.Pow(gf, 1.0 / 2.4) - 0.055 : 12.92 * gf;
             bf = bf > 0.0031308 ? 1.055 * Math.Pow(bf, 1.0 / 2.4) - 0.055 : 12.92 * bf;
 
-            int R = (int)Math.Clamp(Math.Round(rf * 255.0), 0, 255);
-            int G = (int)Math.Clamp(Math.Round(gf * 255.0), 0, 255);
-            int Bl = (int)Math.Clamp(Math.Round(bf * 255.0), 0, 255);
+            double R = Math.Clamp(Math.Round(rf * 255.0), 0, 255);
+            double G = Math.Clamp(Math.Round(gf * 255.0), 0, 255);
+            double Bl = Math.Clamp(Math.Round(bf * 255.0), 0, 255);
 
             return (R, G, Bl);
         }
     }
 
-    public class Pixel
-    {
-        (int, int, int) rgb;
-
-        public Pixel((int, int, int) rgb)
-        {
-            this.rgb = rgb;
-        }
-
-        public (int, int, int) GetRGB()
-        {
-            return rgb;
-        }
-
-        public (double, double, double) GetLAB()
-        {
-            return ColorConvert.RGBToLAB(rgb.Item1, rgb.Item2, rgb.Item3);
-        }
-
-        public static Pixel FromLAB(double L, double A, double B)
-        {
-            return new Pixel(ColorConvert.LABToRGB(L, A, B));
-        }
-    }
-
     public class ImageHelper
     {
-        private Pixel[,] pixelArray;
+        private readonly double[,] r,
+            g,
+            b;
+        private double[,] labL,
+            labA,
+            labB;
+        private bool labCached = false;
+
+        public int Width => r.GetLength(0);
+        public int Height => r.GetLength(1);
 
         public ImageHelper(Image image)
         {
             int w = image.GetWidth();
             int h = image.GetHeight();
-            pixelArray = new Pixel[w, h];
+            r = new double[w, h];
+            g = new double[w, h];
+            b = new double[w, h];
             for (int i = 0; i < w; i++)
             {
                 for (int j = 0; j < h; j++)
                 {
                     Color c = image.GetPixel(i, j);
-                    int r = (int)Math.Round(c.R * 255);
-                    int g = (int)Math.Round(c.G * 255);
-                    int b = (int)Math.Round(c.B * 255);
-                    pixelArray[i, j] = new Pixel((r, g, b));
+                    r[i, j] = Math.Round(c.R * 255);
+                    g[i, j] = Math.Round(c.G * 255);
+                    b[i, j] = Math.Round(c.B * 255);
                 }
             }
         }
 
-        public ImageHelper(ImageHelper image)
+        private ImageHelper(double[,] r, double[,] g, double[,] b)
         {
-            pixelArray = image.pixelArray;
+            this.r = r;
+            this.g = g;
+            this.b = b;
         }
 
-        public ImageHelper(ImageHelper image, int x, int y)
+        private void EnsureLAB()
         {
-            int oldW = image.pixelArray.GetLength(0);
-            int oldH = image.pixelArray.GetLength(1);
-            pixelArray = new Pixel[x, y];
-            for (int i = 0; i < x; i++)
+            if (labCached)
+                return;
+            int w = Width,
+                h = Height;
+            labL = new double[w, h];
+            labA = new double[w, h];
+            labB = new double[w, h];
+            for (int i = 0; i < w; i++)
             {
-                for (int j = 0; j < y; j++)
+                for (int j = 0; j < h; j++)
                 {
-                    int srcX = Math.Min((int)((double)i / x * oldW), oldW - 1);
-                    int srcY = Math.Min((int)((double)j / y * oldH), oldH - 1);
-                    pixelArray[i, j] = image.pixelArray[srcX, srcY];
+                    var (L, A, B) = ColorConvert.RGBToLAB(r[i, j], g[i, j], b[i, j]);
+                    labL[i, j] = L;
+                    labA[i, j] = A;
+                    labB[i, j] = B;
                 }
+            }
+            labCached = true;
+        }
+
+        public double[,] GetChannel(Channel ch)
+        {
+            switch (ch)
+            {
+                case Channel.R:
+                    return r;
+                case Channel.G:
+                    return g;
+                case Channel.B:
+                    return b;
+                case Channel.L:
+                    EnsureLAB();
+                    return labL;
+                case Channel.A:
+                    EnsureLAB();
+                    return labA;
+                case Channel.Lab_B:
+                    EnsureLAB();
+                    return labB;
+                default:
+                    throw new ArgumentException("Unknown channel");
             }
         }
 
-        public ImageHelper(int x, int y)
+        // Returns a new ImageHelper with one RGB channel replaced.
+        public ImageHelper WithChannel(Channel ch, double[,] data)
         {
-            pixelArray = new Pixel[x, y];
+            switch (ch)
+            {
+                case Channel.R:
+                    return new ImageHelper(data, g, b);
+                case Channel.G:
+                    return new ImageHelper(r, data, b);
+                case Channel.B:
+                    return new ImageHelper(r, g, data);
+                case Channel.L:
+                case Channel.A:
+                case Channel.Lab_B:
+                    EnsureLAB();
+                    double[,] newL = ch == Channel.L ? data : labL;
+                    double[,] newA = ch == Channel.A ? data : labA;
+                    double[,] newB = ch == Channel.Lab_B ? data : labB;
+                    return FromLAB(newL, newA, newB);
+                default:
+                    throw new ArgumentException("Unknown channel");
+            }
         }
 
-        public Pixel GetPixel(int x, int y)
+        public static ImageHelper FromLAB(double[,] L, double[,] A, double[,] B)
         {
-            return pixelArray[x, y];
-        }
-
-        public Pixel SetPixel(Pixel p, int x, int y)
-        {
-            Pixel temp = pixelArray[x, y];
-            pixelArray[x, y] = p;
-            return temp;
+            int w = L.GetLength(0),
+                h = L.GetLength(1);
+            double[,] r = new double[w, h];
+            double[,] g = new double[w, h];
+            double[,] b = new double[w, h];
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    var (R, G, Bl) = ColorConvert.LABToRGB(L[i, j], A[i, j], B[i, j]);
+                    r[i, j] = R;
+                    g[i, j] = G;
+                    b[i, j] = Bl;
+                }
+            }
+            var result = new ImageHelper(r, g, b);
+            result.labL = L;
+            result.labA = A;
+            result.labB = B;
+            result.labCached = true;
+            return result;
         }
 
         public Image ToGodotImage()
         {
-            int w = pixelArray.GetLength(0);
-            int h = pixelArray.GetLength(1);
+            int w = Width,
+                h = Height;
             var img = Image.CreateEmpty(w, h, false, Image.Format.Rgb8);
             for (int i = 0; i < w; i++)
             {
                 for (int j = 0; j < h; j++)
                 {
-                    if (pixelArray[i, j] == null)
-                    {
-                        img.SetPixel(i, j, new Color(0, 0, 0));
-                    }
-                    else
-                    {
-                        var (r, g, b) = pixelArray[i, j].GetRGB();
-                        img.SetPixel(i, j, new Color(r / 255f, g / 255f, b / 255f));
-                    }
+                    img.SetPixel(
+                        i,
+                        j,
+                        new Color(
+                            (float)(r[i, j] / 255.0),
+                            (float)(g[i, j] / 255.0),
+                            (float)(b[i, j] / 255.0)
+                        )
+                    );
                 }
             }
             return img;
         }
 
-        public ImageHelper Crop(int x, int y, Vector2 center)
+        public ImageHelper Crop(int w, int h, Vector2 center)
         {
-            var cropped = new ImageHelper(x, y);
-            int startX = (int)center.X - x / 2;
-            int startY = (int)center.Y - y / 2;
-            for (int i = 0; i < x; i++)
+            int startX = (int)center.X - w / 2;
+            int startY = (int)center.Y - h / 2;
+            double[,] cr = new double[w, h];
+            double[,] cg = new double[w, h];
+            double[,] cb = new double[w, h];
+            for (int i = 0; i < w; i++)
             {
-                for (int j = 0; j < y; j++)
+                for (int j = 0; j < h; j++)
                 {
                     int srcX = startX + i;
                     int srcY = startY + j;
-                    if (
-                        srcX >= 0
-                        && srcX < pixelArray.GetLength(0)
-                        && srcY >= 0
-                        && srcY < pixelArray.GetLength(1)
-                    )
+                    if (srcX >= 0 && srcX < Width && srcY >= 0 && srcY < Height)
                     {
-                        cropped.pixelArray[i, j] = pixelArray[srcX, srcY];
+                        cr[i, j] = r[srcX, srcY];
+                        cg[i, j] = g[srcX, srcY];
+                        cb[i, j] = b[srcX, srcY];
                     }
                 }
             }
-            return cropped;
+            return new ImageHelper(cr, cg, cb);
         }
 
-        // upsamples or downsamples image to dimensions x, y
-        public ImageHelper Sample(int x, int y)
+        public ImageHelper Sample(int newW, int newH)
         {
-            return new ImageHelper(this, x, y);
+            int oldW = Width,
+                oldH = Height;
+            double[,] sr = new double[newW, newH];
+            double[,] sg = new double[newW, newH];
+            double[,] sb = new double[newW, newH];
+            for (int i = 0; i < newW; i++)
+            {
+                for (int j = 0; j < newH; j++)
+                {
+                    int srcX = Math.Min((int)((double)i / newW * oldW), oldW - 1);
+                    int srcY = Math.Min((int)((double)j / newH * oldH), oldH - 1);
+                    sr[i, j] = r[srcX, srcY];
+                    sg[i, j] = g[srcX, srcY];
+                    sb[i, j] = b[srcX, srcY];
+                }
+            }
+            return new ImageHelper(sr, sg, sb);
         }
     }
 }
